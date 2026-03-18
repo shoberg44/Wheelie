771847aeefca5fc6efdfac56225e2dc1846f3e62
Steven Hoberg <steven.hoberg1@gmail.com>
2026-03-18 19:41:08 -0400

    simp down hello world with AppState

--- Wheelie/src-tauri/src/lib.rs
+++ Wheelie/src-tauri/src/lib.rs
@@ -1,14 +1,33 @@
+use std::sync::Mutex;
+use tauri::State; // used to manage the apps state
+
+struct AppState {
+    click_count: Mutex<i32>,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+const RANDOM_TITLES: [&str; 3] = ["hi","hello","greetings"];
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_title(state: State<AppState>) -> String {
+    let mut count = state.click_count.lock().unwrap(); // re-locks when function ends RAII
+    *count = (*count + 1) % (RANDOM_TITLES.len() as i32); // .len() is u32
+    format!("{}",RANDOM_TITLES[(*count) as usize])
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // tells the process what to put in AppState at compile time 
+        .manage(AppState {
+            click_count: Mutex::new(0),
+        })
+        // file reading
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // allows us to use these functions from front end
+        .invoke_handler(tauri::generate_handler![get_title])
+        // generate OS window 
         .run(tauri::generate_context!())
+        // crash handling
         .expect("error while running tauri application");
 }
--- Wheelie/src/App.tsx
+++ Wheelie/src/App.tsx
@@ -1,49 +1,32 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [titleMsg, setTitleMsg] = useState("Waiting for backend...");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+  async function newTitle() {
+    let response: string = await invoke("get_title");
+    setTitleMsg(response);
+
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
+      <h1>
+        Hello :)
+      </h1>
+      
+      <button 
+        onClick={newTitle} 
+        className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
+        Click Me!
+      </button>
+
+      <p>
+        {titleMsg}
+      </p>
     </main>
   );
 }
